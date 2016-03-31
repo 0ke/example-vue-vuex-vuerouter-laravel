c512a7628c323bdb0227477d9d29436c27729c8a
Jonas Vanderhaegen <jonas@uni-t.be>
2016-03-31 20:00:42 +0200

    update modelfactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,13 +9,33 @@
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
 |
-*/
+ */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
+        'username' => $faker->name,
+        'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Candidate::class, function (Faker\Generator $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'age' => $faker->numberBetween(20, 30),
+        'state' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'email' => $faker->freeEmail,
+        'experience' => $faker->paragraph(1),
+        'motivation' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Changelog::class, function (Faker\Generator $faker) {
+    return [
+        'version_id' => $faker->numberBetween(1, 100),
+        'changes' => json_encode($faker->paragraphs(3)),
     ];
 });
