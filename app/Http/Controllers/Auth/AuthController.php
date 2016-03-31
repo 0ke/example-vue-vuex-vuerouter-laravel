edb4c168ffea9983e27d17e63e8f29034dd15d7e
Jonas Vanderhaegen <jonas@uni-t.be>
2016-03-31 20:20:08 +0200

    modify authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helpers\Auth\AuthenticatesAndRegistersUsers;
+use App\Helpers\Auth\ThrottlesLogins;
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository as Repository;
 use App\User;
 use Validator;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
@@ -19,7 +20,7 @@ class AuthController extends Controller
     | authentication of existing users. By default, this controller uses
     | a simple trait to add these behaviors. Why don't you explore it?
     |
-    */
+     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
@@ -29,15 +30,18 @@ class AuthController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+    protected $loginPath = 'auth/login';
+    protected $redirectPath = '/';
 
     /**
      * Create a new authentication controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Repository $repo)
     {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        $this->repo = $repo;
+        $this->middleware('guest', ['except' => 'getLogout']);
     }
 
     /**
