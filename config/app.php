4514a1a36407a6c3c864a0f5e0ce2db68b0ec836
Jonas Vanderhaegen <jonas@uni-t.be>
2016-03-31 20:22:30 +0200

    okey nevermind JWT

--- config/app.php
+++ config/app.php
@@ -158,8 +158,6 @@ return [
 
         App\Providers\ViewComposerServiceProvider::class,
         App\Providers\ResponseMacroServiceProvider::class,
-
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
     ],
 
     /*
@@ -205,9 +203,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
